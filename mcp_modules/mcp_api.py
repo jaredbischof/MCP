07147767a16edc61f663b8ad94b7e42e607c9482
Jared Bischof <jbischof@gmail.com>
2013-03-18 14:41:25 -0500

    Updated code to read config file in json format.  Updated component 'state' to be more consistent across all components.  Added stop and restart methods to mcp_api component.

--- mcp_modules/mcp_api.py
+++ mcp_modules/mcp_api.py
@@ -1,17 +1,18 @@
-import glob, json, sys, time
+import glob, json, os, sys, time
 from subsystem import subsystem
 
 class mcp_api(subsystem):
-    actions = "start"
+    actions = "start, stop, restart"
 
     def __init__(self, MCP_dir):
         subsystem.__init__(self, MCP_dir)
         self.state = { 'resource':self.__class__.__name__,
-                       'date':time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+                       'updated':time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+                       'url':self.json_conf['global']['apiurl'] + "/" + str(self.json_conf['mcp_api']['version']) + "/" + self.__class__.__name__,
                        'status':'online'
                      }
-        self.apidir = self.parser.get('mcp_api', 'dir') + "/" + self.parser.get('mcp_api', 'version')
-        self.services = self.parser.get('global', 'services')
+        self.apidir = self.json_conf['mcp_api']['dir'] + "/" + str(self.json_conf['mcp_api']['version'])
+        self.services = self.json_conf['global']['services']
 
     def start(self):
         files = glob.glob(self.apidir+"/*")
@@ -19,7 +20,7 @@ class mcp_api(subsystem):
             sys.stderr.write("ERROR: Cannot initialize mcp_api because API directory (" + self.apidir + ") is not empty\n")
             return 0
 
-        for service in self.services.split(','):
+        for service in self.services:
             service = service.strip();
             module = __import__(service)
             myclass = getattr(module, service)
@@ -28,3 +29,15 @@ class mcp_api(subsystem):
             jstate = json.dumps(myservice.get_state())
             f = open(self.apidir + "/" + service, 'w')
             f.write(jstate)
+
+    def stop(self):
+        for file_object in os.listdir(self.apidir):
+            file_object_path = os.path.join(self.apidir, file_object)
+            if os.path.isfile(file_object_path):
+                os.unlink(file_object_path)
+            else:
+                shutil.rmtree(file_object_path)
+
+    def restart(self):
+        self.stop()
+        self.start()
