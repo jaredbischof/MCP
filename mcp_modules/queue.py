c7d86d1914b3bad71eca54f27945a1bfb873d95e
Jared Bischof <jbischof@gmail.com>
2013-03-23 00:47:17 -0500

    Added descriptions to each subsystems' actions so the command line help can be more informative.  Also, edited queue resource to use an array of named queues so that the command can be extended to include any new queues that we might have in the future.

--- mcp_modules/queue.py
+++ mcp_modules/queue.py
@@ -5,36 +5,44 @@ class queue(subsystem):
 
     def __init__(self, MCP_path):
         subsystem.__init__(self, MCP_path)
+        self.queues = [ 'batch', 'fast' ]
+        self.state['status'] = {}
 
         # getting status of queues
-        sout, serr = self.run_cmd("/usr/local/bin/qstat -Q batch")
-        lines = sout.splitlines()
-        batch_status = 'online' if lines[len(lines)-1].split()[3] == 'yes' else 'offline'
-
-        sout, serr = self.run_cmd("/usr/local/bin/qstat -Q fast")
-        lines = sout.splitlines()
-        fast_status = 'online' if lines[len(lines)-1].split()[3] == 'yes' else 'offline'
-
-        self.state['status'] = { 'batch': batch_status, 'fast': fast_status }
+        for queue in self.queues:
+            sout, serr = self.run_cmd("/usr/local/bin/qstat -Q " + queue)
+            lines = sout.splitlines()
+            status = 'online' if lines[len(lines)-1].split()[3] == 'yes' else 'offline'
+            self.state['status'][queue] = status
 
     def start(self, params):
         action = 'start'
-        self.parse_action_params(action, [], {}, params)
+        action_params = [ 'queue_name' ]
+        action_param_settings = { 'queue_name' : [ str, "name of the queue to run qstart on ('" + "', '".join(self.state['status']) + "')"] }
+        desc = "description: this action runs qstart on the selected queue ('" + "', '".join(self.state['status']) + "')"
+        self.parse_action_params(action, action_params, action_param_settings, desc, params)
+
         if self.check_userhost() == -1:
             self.pass_mcp_cmd(action, params)
             return 0
 
-        print "Starting nagasaki pipeline:"
-        sout, serr = self.run_cmd("/usr/local/bin/qstart batch")
-        print "nagasaki pipeline started!"
+        queue = params[0]
+        print "ACTION: Starting " + queue + " queue"
+        sout, serr = self.run_cmd("/usr/local/bin/qstart " + queue)
+        print "SUCCESS: " + queue + " queue started!"
 
     def stop(self, params):
         action = 'stop'
-        self.parse_action_params(action, [], {}, params)
+        action_params = [ 'queue_name' ]
+        action_param_settings = { 'queue_name' : [ str, "name of the queue to run qstop on ('" + "', '".join(self.state['status']) + "')"] }
+        desc = "description: this action runs qstop on the selected queue ('" + "', '".join(self.state['status']) + "')"
+        self.parse_action_params(action, action_params, action_param_settings, desc, params)
+
         if self.check_userhost() == -1:
             self.pass_mcp_cmd(action, params)
             return 0
 
-        print "Stopping nagasaki pipeline:"
-        sout, serr = self.run_cmd("/usr/local/bin/qstop batch")
-        print "nagasaki pipeline stopped!"
+        queue = params[0]
+        print "ACTION: Stopping " + queue + " queue"
+        sout, serr = self.run_cmd("/usr/local/bin/qstop " + queue)
+        print "SUCCESS: " + queue + " queue stopped!"
