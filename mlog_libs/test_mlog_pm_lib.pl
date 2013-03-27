4cad461f11a8d39e742ad01bd530f6658aa98148
Jared Bischof <jbischof@gmail.com>
2013-03-27 11:28:41 -0500

    Moving mlog test scripts to mlog_libs directory.

---
+++ mlog_libs/test_mlog_pm_lib.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+
+use strict;
+use mlog;
+
+################################################################################
+
+init_mlog("pipeline");
+print "log_level = ".get_log_level()."\n";
+
+logit(2, "my test message at log level 2 with no constraints");
+logit(3, "my test message at log level 3 with no constraints");
+logit(4, "my test message at log level 4 with no constraints");
+
+################################################################################
+
+my %log_constraints = ('job' => '3', 'stage' => '350');
+init_mlog("pipeline", \%log_constraints);
+print "log_level = ".get_log_level()."\n";
+
+logit(2, "my test message at log level 2 with constraints job=3 and stage=350");
+logit(3, "my test message at log level 3 with constraints job=3 and stage=350");
+logit(4, "my test message at log level 4 with constraints job=3 and stage=350");
+
+################################################################################
+
+set_log_level(5);
+print "log_level = ".get_log_level()."\n";
+
+logit(2, "my test message at log level 2 with constraints job=3 and stage=350");
+logit(3, "my test message at log level 3 with constraints job=3 and stage=350");
+logit(4, "my test message at log level 4 with constraints job=3 and stage=350");
+
+################################################################################
