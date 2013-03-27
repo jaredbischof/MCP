8dee4c26bb3c0b5a88488d578740ed25536a2caf
Jared Bischof <jbischof@gmail.com>
2013-03-27 14:33:23 -0500

    Updated mlog.py text script to use new convention for calling mlog.

---
+++ mlog_libs/test_mlog_py_lib.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from mlog import mlog
+
+################################################################################
+
+m = mlog("pipeline")
+print "log_level = " + str(m.get_log_level())
+
+m.logit(2, "my test message at log level 2 with no constraints")
+m.logit(3, "my test message at log level 2 with no constraints")
+m.logit(4, "my test message at log level 2 with no constraints")
+
+################################################################################
+
+log_constraints = { 'job' : '3', 'stage' : '350' }
+m = mlog("pipeline", log_constraints)
+print "log_level = " + str(m.get_log_level())
+
+m.logit(2, "my test message at log level 2 with constraints job=3 and stage=350");
+m.logit(3, "my test message at log level 3 with constraints job=3 and stage=350");
+m.logit(4, "my test message at log level 4 with constraints job=3 and stage=350");
+
+################################################################################
+
+m.set_log_level(5)
+print "log_level = " + str(m.get_log_level())
+
+m.logit(2, "my test message at log level 2 with constraints job=3 and stage=350");
+m.logit(3, "my test message at log level 3 with constraints job=3 and stage=350");
+m.logit(4, "my test message at log level 4 with constraints job=3 and stage=350");
+
+################################################################################
+
+m.logit('emergency', "this program is finished")
