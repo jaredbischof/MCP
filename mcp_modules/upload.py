cac97a476dc026048780a4eba4f456476b27a315
Jared Bischof <jbischof@gmail.com>
2013-03-26 22:29:05 -0500

    Added syslog messages to record actions. Also added sort_keys to json dumps for consistency in api pages. Also added update_status() method.

--- mcp_modules/upload.py
+++ mcp_modules/upload.py
@@ -2,13 +2,15 @@ import os, json, sys
 from subsystem import subsystem
 
 class upload(subsystem):
-    actions = [ 'lock_page', 'unlock_page', 'log' ]
+    actions = [ 'lock_page', 'unlock_page', 'set_log', 'delete_log' ]
 
     def __init__(self, MCP_path):
         subsystem.__init__(self, MCP_path)
-        self.state['status'] = {}
-
         self.lock_file = self.json_conf['upload']['lock_dir'] + "/upload.lock"
+        self.state['status'] = {}
+        self.state['status']['page'] = ''
+
+    def update_status(self):
         if os.path.isfile(self.lock_file):
             self.state['status']['page'] = "offline"
         else:
@@ -23,6 +25,8 @@ class upload(subsystem):
             self.pass_mcp_cmd(action, params)
             return 0
 
+        self.log_msg("ACTION : attempting to disable upload page")
+
         fh = file(self.lock_file, 'a')
         try:
             os.utime(self.lock_file, None)
@@ -30,9 +34,10 @@ class upload(subsystem):
             fh.close()
 
         self.state['status']['page'] = "offline"
-        jstate = json.dumps(self.state)
+        jstate = json.dumps(self.state, sort_keys=True)
         f = open(self.apidir + "/" + self.__class__.__name__, 'w')
         f.write(jstate)
+        self.log_msg("SUCCESS : upload page disabled")
 
     def unlock_page(self, params):
         action = 'unlock_page'
@@ -43,10 +48,13 @@ class upload(subsystem):
             self.pass_mcp_cmd(action, params)
             return 0
 
+        self.log_msg("ACTION : attempting to enable upload page")
+
         if os.path.isfile(self.lock_file):
             os.unlink(self.lock_file)
 
         self.state['status']['page'] = "online"
-        jstate = json.dumps(self.state)
+        jstate = json.dumps(self.state, sort_keys=True)
         f = open(self.apidir + "/" + self.__class__.__name__, 'w')
         f.write(jstate)
+        self.log_msg("SUCCESS : upload page enabled")
